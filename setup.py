f6831f79ce7f7d4a29828534b2db729599894543
Monika Sulik <monika.sulik@tangentsnowball.com>
2015-01-07 15:03:13 +0000

    Add dependencies to setup.py file

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ setup(name="barebones-cms",
       version="0.0.1",
       description="A very simple service orientated CMS for django",
       packages=['barebones_cms'],
-      include_package_data=True)
+      include_package_data=True,
+      install_requires=[
+        'django-mptt==0.6.1',
+        'django==1.7',
+        'django-extensions==1.4.9',
+        'psycopg2==2.5.4',
+        'Werkzeug==0.9.6',
+        'ipdb==0.8',
+      ])
