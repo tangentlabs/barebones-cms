e79da92ba0a04ad663f391fd3c24f002e363bc61
Monika Sulik <monika.sulik@tangentsnowball.com>
2015-01-07 11:45:20 +0000

    setup.py for barebones_cms

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="barebones-cms",
+      version="0.0.1",
+      description="A very simple service orientated CMS for django",
+      packages=['barebones_cms'],
+      include_package_data=True)
